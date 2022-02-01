eea5ebd6942202fa5ce2b254207322f67015d7a2
Aminu Mukhtar Mado <madoaminu@gmail.com>
2022-02-01 16:35:14 +0100

    imported the images used

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import darkSouls_Src from "../public/Dark-Souls.jpg";
+import metalGear_Src from "../public/Metal-Gear-Solid.jpg";
+import bloodBorne_Src from "../public/BloodBorne.jpg";
+import finalFantasy_Src from "../public/Final-Fantasy-X.png";
+
 export default [
   {
     title: "Metal Gear Solid 2",
@@ -8,7 +13,7 @@ export default [
     Link: "https://en.wikipedia.org/wiki/Metal_Gear_Solid_2:_Sons_of_Liberty",
     description:
       "The story revolves around the Big Shell, a massive offshore clean-up facility seized by a group of terrorists who call themselves the Sons of Liberty. They demand an enormous ransom in exchange for the life of the President of the United States and threaten to destroy the facility and create a cataclysmic environmental disaster if their demands are not met. The motives and identities of many of the antagonists and allies change throughout the course of the game, as the protagonists discover a world-shaking conspiracy constructed by a powerful organization known as the Patriots.",
-    imageUrl: "./public/Metal-Gear-Solid.jpg",
+    imageUrl: metalGear_Src,
   },
   {
     title: "Dark Souls",
@@ -19,7 +24,7 @@ export default [
     Link: "https://en.wikipedia.org/wiki/Dark_Souls",
     description:
       "Dark Souls employs minimalist storytelling to convey its plot and lore. Historical events in the world and their significance are often implicit or left to player interpretation rather than fully shown or explained. Most of the story is given to the player through dialogue from non-player characters, flavour text from items, and world design.",
-    imageUrl: "./public/Dark-Souls.jpg",
+    imageUrl: darkSouls_Src,
   },
   {
     title: "BloodBorne",
@@ -29,7 +34,7 @@ export default [
     Genre: "Action role-playing",
     Link: "https://en.wikipedia.org/wiki/Bloodborne",
     description: `Bloodborne takes place in Yharnam, a decrepit Gothic city known for its medical advances around the practice of blood ministration. Over the years, many travellers journey to the city seeking the remedy to cure their afflictions; the player's character journeys to Yharnam seeking the cure, something known as Paleblood, for an unspecified illness. However, upon arriving in the city, it is discovered that Yharnam is plagued with an endemic illness that has transformed most of its citizens into bestial creatures. The player must navigate the streets of Yharnam during the night of The Hunt and overcome its violently deranged inhabitants and horrifying monsters to stop the source of the plague and escape the Nightmare. Long ago, the residents of Yharnam began worshipping ancient, eldritch cosmic beings known as the Great Ones after scholars from the College of Byrgenwerth discovered something referred to as a "medium" in the ruins of an ancient, highly advanced civilization, on top of which Yharnam was built. The Great Ones provided the healing blood Yharnam was famous for, which is also the source of the plague.`,
-    imageUrl: "./public/BloodBorne.jpg",
+    imageUrl: bloodBorne_Src,
   },
   {
     title: "Final Fantasy X",
@@ -40,6 +45,6 @@ export default [
     Link: "https://en.wikipedia.org/wiki/Final_Fantasy_X",
     description:
       "Set in the fantasy world of Spira, a setting influenced by the South Pacific, Thailand and Japan,[1] the game's story revolves around a group of adventurers and their quest to defeat a rampaging monster known as Sin. The player character is Tidus, a star athlete in the fictional sport of blitzball, who finds himself in Spira after Sin destroyed his home city of Zanarkand. Shortly after arriving to Spira, Tidus becomes a guardian to summoner Yuna to destroy Sin upon learning its true identity is that of his missing father, Jecht.",
-    imageUrl: "./public/Final-Fantasy-X.png",
+    imageUrl: finalFantasy_Src,
   },
 ];
